63b4e09c1adcf5b5ae9f166dc576062d6a212746
cyber-nerd21 <amoghdeodhar886@gmail.com>
2026-04-06 17:46:56 +0530

    fix: use gemini-embedding-model-001

--- Backend/rag/embedder.py
+++ Backend/rag/embedder.py
@@ -4,13 +4,11 @@ import os
 
 load_dotenv()
 
-client = genai.Client(
-    api_key=os.getenv("GEMINI_API_KEY"),
-)
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 def get_embeddings(text: str) -> list:
     response = client.models.embed_content(
-        model="gemini-embedding-exp-03-07",
+        model="gemini-embedding-001",
         contents=text
     )
     return response.embeddings[0].values
